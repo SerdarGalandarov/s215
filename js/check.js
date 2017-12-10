bf4500510b664632a5dddf93aaf2814ba2f3dcfc
serdar <ainane@inbox.ru>
2017-12-10 21:17:45 +0600

    add js

---
+++ js/check.js
@@ -0,0 +1,13 @@
+var year = visikisniy(2012); 
+console.log(year);
+
+function visikisniy(year) {
+    if(year%4 == 0) {
+        return true;
+    }
+    else {
+        return false;
+        }
+    }
+
+
---
+++ js/list.js
@@ -0,0 +1,5 @@
+var i; 
+for (i=2004; i<2100; i=i+4) {
+	console.log('number:');
+  console.log(i);
+}
